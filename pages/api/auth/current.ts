8b31804e62d01565cc72beb6d3cc92305b1c876d
Marka <g.lkhagvadelger@gmail.com>
2023-12-27 20:28:11 +0800

    Update current.ts

--- pages/api/auth/current.ts
+++ pages/api/auth/current.ts
@@ -4,11 +4,12 @@ import { ERROR_MESSAGES } from "@util/errors";
 const handler = createHandler();
 
 handler.get(async (req, res) => {
-  if (req.user) {
-    res.sendSuccess(req.user);
-  } else {
-    res.sendError(401, ERROR_MESSAGES.UNAUTHORIZED, "not-logged-in");
-  }
+  // if (req.user) {
+  //   res.sendSuccess(req.user);
+  // } else {
+  //   res.sendError(401, ERROR_MESSAGES.UNAUTHORIZED, "not-logged-in");
+  // }
+  res.sendError(401, ERROR_MESSAGES.UNAUTHORIZED, "not-logged-in");
 });
 
 export default handler;
